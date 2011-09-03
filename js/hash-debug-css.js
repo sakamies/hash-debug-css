d63ae7c5d6321b4af62b60af66d5f08048489156
sakamies <sakamies@gmail.com>
2011-09-03 04:22:14 +0300

    Added decoding url entities and catching invalid selector errors

--- js/hash-debug-css.js
+++ js/hash-debug-css.js
@@ -1,12 +1,47 @@
-$(document).ready( function() {
-	if( window.location.hash && window.location.hash.match(/\?debug((+.))$/)) {
-		var selector = window.location.hash.replace('#debug(', '').replace(')', '');
-		if (selector != '') {
-			document.querySelectorAll(selector).each?.style.outline = '1px solid purple';
+window.onload = function(){ //using onload because we want images to be loaded when checking layout
+//	if(window.location.hash.match(/^#debug/)) {
+	if(window.location.hash.indexOf('#debug(') !== -1 && window.location.hash.indexOf(')') > 6) {
+
+		var selector = window.location.hash
+						.replace('#debug', '')
+						.replace('(', '')
+						.replace(')', '')
+						.replace(/%2A/g, '*')
+						.replace(/%23/g, '#')
+						//attribute selectors
+						.replace(/%5B/g, '[')
+						.replace(/%3D/g, '=')
+						.replace(/%7E/g, '~')
+						.replace(/%22/g, '"')
+						.replace(/%27/g, "'")
+						.replace(/%5D/g, ']')
+						//combinators
+						.replace(/%20/g, ' ')
+						.replace(/%2B/g, '+')
+						.replace(/%3E/g, '>')
+						.replace(/%3E/g, '>')
+						;
+		if (!selector) { selector = "body *"};
+
+		try {
+			var elements = document.querySelectorAll(selector);
+			var element;
+			// -> https://developer.mozilla.org/En/DOM/NodeList#Example
+			for (element = 0; element < elements.length; ++element) {
+				elements[element].style.cssText += 'outline: 1px solid #FF00FF;';
+				console.log(elements[element]);
+			}
+			console.log('#debug css outlines added');
+			console.log('selector: ' + selector);
 		}
-		else {
-			/* without a selector, apply an outline to everything */
-		};
-		console.log('debug selector: ' + );
+		catch(error) {
+			console.error('#debug css outlines not added');
+			if (error.code == 12) {
+				console.error('Invalid selector: ' + selector);
+			}
+			else {
+				console.error(error);
+			}
+		}
 	}
-});
+};
